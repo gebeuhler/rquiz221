1d05e0fc998dd2ab8996565c6e32e496c0581686
gebeuhler <gebeuhler@gmail.com>
2014-04-27 13:55:03 -0500

    refactored to remove Team class and instead use only HashMap

--- src/TeamBot.java
+++ src/TeamBot.java
@@ -3,48 +3,11 @@ import org.jibble.pircbot.*;
 
 public class TeamBot extends PircBot
 {
-	private class Team
-	{
-		private String name;
-		private List<String> members;
-
-		public Team()
-		{
-			this.members = new ArrayList<String>();
-		}
-
-		public Team( String name,  List<String> members)
-		{
-			this.name = name;
-			this.members = members;
-		}
-
-		public String getName()
-		{
-			return this.name;
-		}
-
-		public void setName( String name)
-		{
-			this.name = name;
-		}
-
-		public List<String> getMembers()
-		{
-			return this.members;
-		}
-
-		public void setMembers( List<String> members)
-		{
-			this.members = members;
-		}
-	}
-
-	private Map<String, Team> teams;
+	private Map<String, List<String>> teams;
 
 	public TeamBot()
 	{
-		this.teams = new HashMap<String, Team>();
+		this.teams = new HashMap<String, List<String>>();
 		this.setName("TeamBot1234");
 	}
 
@@ -63,102 +26,150 @@ public class TeamBot extends PircBot
 			//TODO: Move this logic to?
 			String[] splitMessage = message.split("\\s+");
 			if(createTeam(sender, splitMessage[2]))
-				sendMessage(channel, "team " + splitMessage[2] + " created!");
+				sendMessage(channel, sender + ": team " + splitMessage[2] + " created!");
 			else
-				sendMessage(channel, "team " + splitMessage[2] + " already exists!");
+				sendMessage(channel, sender + ": team " + splitMessage[2] + " already exists!");
 		}
 		//show teams
 		if(message.matches("show teams"))
 		{
-			sendMessage(channel, showTeams());
+			if(teams.isEmpty())
+				sendMessage(channel, sender + ": no teams exist!");
+			else	
+				sendMessage(channel, showTeams());
 		}
 		//show team <teamname>
 		if(message.matches("show team \\w+"))
 		{
 			//TODO: Move this logic to?
-			 String[] splitMessage = message.split("\\s+");
-			sendMessage(channel, showTeam(splitMessage[2]));
+			String[] splitMessage = message.split("\\s+");
+			String teamName = splitMessage[2];
+			
+			if(doesTeamExist(teamName))
+				sendMessage(channel, sender + ": " + getTeamMembers(teamName));
+			else
+				sendMessage(channel, sender + ": team " + teamName + " does not exist!");
 		}
 		//join team <teamname>
 		if(message.matches("join team \\w+"))
 		{
 			String[] splitMessage = message.split("\\s+");
-			if(joinTeam(splitMessage[2], sender))
-				sendMessage(channel, sender + " has joined team " + splitMessage[2] + "!");
-			else
-				sendMessage(channel, sender + " cannot join team " + splitMessage[2] + "!");
+			String teamName = splitMessage[2];
+			
+			if(doesTeamExist(teamName))
+			{
+				if(joinTeam(teamName, sender))
+					sendMessage(channel, sender + ": you have joined team " + teamName + "!");
+				else
+					sendMessage(channel, sender + ": you already belong to team " + teamName + "!");
+			}
+			else		
+				sendMessage(channel, sender + ": team " + teamName + " does not exist!");
 		}
+		/*
+		//leave team
+		if(message.matches("leave team"))
+		{
+			if(leaveTeam(sender))
+			//only input is sender
+		}
+		*/
     }
 
-	private boolean createTeam( String memberName,  String teamName)
-	{
-		Team targetTeam = teams.get(teamName);		
-		if(targetTeam != null)
-			//team already exists; exit
-			return false;
-
-		//create member list
-		 List<String> members = new ArrayList<String>();
-		members.add(memberName);
-		//create team and add member to it
-		Team newTeam = new Team(teamName, members);
-		teams.put(teamName, newTeam);
-
-		return true;
-	}
-
-	private String showTeams()
-	{
-		String retval = "";
-		//TODO: condense this duplicate code A
-		for(String teamName : teams.keySet())
-		{
-			retval += teamName + ", ";
-		}
-		//remove final comma space, kind of a hack think of something better!
-		retval = retval.substring(0, retval.length()-2);
-		return retval;
-	}
-
-	private String showTeam(String teamName)
-	{
-		String retval = "";
-		if(teams.get(teamName) == null)
-		{
-			retval = "Team does not exist!";
-		}
-		else
-		{
-			retval += "team " + teams.get(teamName).getName() + ": ";
-			//TODO: condense this duplicate code A
-			for(String memberName : teams.get(teamName).getMembers())
-			{
-				retval += memberName + ", ";
-			}
-			//remove final comma space, kind of a hack think of something better!
-			retval = retval.substring(0, retval.length()-2);
-		}
-		return retval;
-	}
-	
-	private boolean joinTeam(String teamName, String memberName)
+	private boolean createTeam(String memberName,  String teamName)
 	{
 		if(teams.get(teamName) != null)
 		{
-			teams.get(teamName).getMembers().add(memberName);
-			return true;
+			//team already exists, exit
+			return false;
 		}
 		
-		return false;
+		//create new team
+		List<String> members = new ArrayList<String>();
+		members.add(memberName);
+		teams.put(teamName, members);
+		return true;
+	}
+	
+	//assumes teams exist already
+	private String showTeams()
+	{
+		String teamList = "";
+		Set<String> teamNames = teams.keySet();
+		int size = teamNames.size();
+		
+		for(String teamName : teamNames)
+		{
+			teamList += teamName;
+			if(--size != 0)
+				teamList += ", ";
+		}
+		
+		return teamList;
+	}
+
+	//assume team exists already
+	private String getTeamMembers(String teamName)
+	{
+		String memberList = "team " + teamName + ": ";
+		List<String> members = teams.get(teamName);
+		int size = members.size();
+		
+		for(String memberName : members)
+		{
+			memberList += memberName;
+			if(--size != 0)
+				memberList += ", ";
+		}
+		
+		return memberList;
+	}
+	
+	//assumes team already exists!!
+	private boolean joinTeam(String teamName, String memberName)
+	{
+		List <String> members = teams.get(teamName);
+	
+		if(members.contains(memberName))
+			return false;
+			
+		members.add(memberName);
+		return true;
 		
 	}
+	
+	private boolean doesTeamExist(String teamName)
+	{
+		if(teams.get(teamName) != null)
+		{
+			return true;
+		}
+		return false;
+	}
+	/*
+	private boolean leaveTeam(String memberName)
+	{
+		if(teams.get(teamName) != null)
+		{
+			teams.get(teamName).getMembers().remove(memberName)
+			return true;
+		}
+		return false;
+	}
+	*/
+
+//data structure currently: map<String teamName, Team>
+//Team is: name, list of members
+//simplify to: map<String teamName, List<String>>
+//worth a try in new branch
+
 
 /*
 create team <teamname> DONE
-join <teamname> DONE
+join <teamname> 
 leave team
 show teams DONE
-show team <teamname> DONE
+show team <teamname> 
 show my team
 delete team <teamname>
 reset
